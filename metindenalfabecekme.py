d9314bb5a25cc769709d20df6fbca17bed6f0c49
Your Name <you@example.com>
2019-09-26 23:34:39 +0300

    python3

--- metindenalfabecekme.py
+++ metindenalfabecekme.py
@@ -2,9 +2,15 @@
 
 
 
-# herhangi bir metinden alfabe cekme
+
 
 a = "python nesne tabanlı bir programlama dilidir"
 
-
+# herhangi bir metinden alfabe cekme
 print (a[2])
+
+#herhangi bir metinden kelime cekme
+print (a[7:13])
+
+#herhangi bir metinden atlamalı olarak alfabe cekme
+print(a[::2])
