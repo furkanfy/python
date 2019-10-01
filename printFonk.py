30b26701c3e996fee06f10892b67e666140e0f54
Your Name <you@example.com>
2019-10-01 18:07:17 +0300

    python4

---
+++ printFonk.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+"""ekrana yazdırma"""
+
+a= "python"
+b= 2
+c=3.15
+
+print(a,b,c)
+
+"""type bulma"""
+
+print(type(a),type(b),type(c))
---
+++ printParametre.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""sep parametresi"""
+
+print("ahmet","ali",sep='/')
+
+"""end parametresi"""
+
+print("veli","hüseyin",end= '---->')
+print(20,21)
+
+""" .format() parametresi"""
+
+print("{}'nin kardeşi {}".format("ali","veli"))
+
